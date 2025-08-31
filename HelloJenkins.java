aad373b97cb94e44643f79805c43d5ec16883ae5
Agney Komath <agneybiju@gmail.com>
2025-09-01 04:01:37 +0530

    Updated Message

--- HelloJenkins.java
+++ HelloJenkins.java
@@ -1,5 +1,5 @@
 public class HelloJenkins {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins + Git Updated!");
+        System.out.println("Hello, Jenkins + Git Update!");
     }
 }
